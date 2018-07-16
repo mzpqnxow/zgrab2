6499f6ba6f5ff10a06610267b6a6a625b692b436
Clayton Zimmerman <clayzim118@gmail.com>
2018-07-16 14:35:05 -0400

    Implements TestReadAllAttributes fully.

--- modules/ipp/scanner.go
+++ modules/ipp/scanner.go
@@ -28,6 +28,8 @@ const (
 	PrinterURISupported string = "printer-uri-supported"
 )
 
+// TODO: Standardize more errors as globals (and maybe figure out something better than global vars)
+
 var (
 	// ErrRedirLocalhost is returned when an HTTP redirect points to localhost,
 	// unless FollowLocalhostRedirects is set.
@@ -39,6 +41,12 @@ var (
 
 	// TODO: Explain this error
 	ErrVersionNotSupported = errors.New("IPP version not supported")
+
+	// TODO: Explain this error
+    ErrBodyTooShort = errors.New("Fewer body bytes read than expected.")
+
+    // TODO: Explain this error
+    ErrInvalidLength = errors.New("Reported field length runs out of bounds.")
 
 	Versions = []version{{Major: 2, Minor: 1}, {Major: 2, Minor: 0}, {Major: 1, Minor: 1}, {Major: 1, Minor: 0}}
 	AttributesCharset = []byte{0x47, 0x00, 0x12, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x2d, 0x63, 0x68, 0x61, 0x72, 0x73, 0x65, 0x74}
@@ -222,7 +230,7 @@ func shouldReturnAttrs(length, soFar, size, upperBound int) (bool, error) {
 		if size >= upperBound {
 			return true, nil
 		}
-		return true, zgrab2.NewScanError(zgrab2.SCAN_PROTOCOL_ERROR, errors.New("Reported field length runs out of bounds."))
+		return true, zgrab2.NewScanError(zgrab2.SCAN_PROTOCOL_ERROR, ErrInvalidLength)
 
 	}
 	return false, nil
@@ -230,7 +238,7 @@ func shouldReturnAttrs(length, soFar, size, upperBound int) (bool, error) {
 
 func detectReadBodyError(err error) error {
 	if err == io.EOF || err == io.ErrUnexpectedEOF {
-		return zgrab2.NewScanError(zgrab2.SCAN_PROTOCOL_ERROR, errors.New("Fewer body bytes read than expected."))
+		return zgrab2.NewScanError(zgrab2.SCAN_PROTOCOL_ERROR, ErrBodyTooShort)
 	}
 	return zgrab2.NewScanError(zgrab2.TryGetScanStatus(err), err)
 }
